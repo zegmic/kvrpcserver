b5bbb9b6f0b24fbe3d762e2be5e50c605d4763ec
Michal Zeglarski <michal.zeglarski@gmail.com>
2024-01-26 11:37:48 +0100

    Handling more JSON RPC errors

--- src/rpc.rs
+++ src/rpc.rs
@@ -36,7 +36,7 @@ struct JSONRPCErrorResponse {
 
 enum JSONRPCResponse {
     Ok(JSONRPCOkResponse),
-    Err(JSONRPCErrorResponse)
+    Err(JSONRPCErrorResponse),
 }
 
 impl Responder for JSONRPCResponse {
@@ -54,32 +54,50 @@ impl Responder for JSONRPCResponse {
     }
 }
 
+fn rpc_error(id: i32, code: i32, message: &str) -> JSONRPCResponse {
+    JSONRPCResponse::Err(JSONRPCErrorResponse {
+        jsonrpc: "2.0".to_string(),
+        error: JSONRPCErrorDetails { code, message: message.to_string() },
+        id,
+    })
+}
 
 #[post("/")]
-async fn index(svc: web::Data<Mutex<KVService>>, req: HttpRequest, json_request: web::Json<JSONRPCRequest>) ->  JSONRPCResponse {
-    println!("{}", req.connection_info().realip_remote_addr().unwrap());
+async fn index(svc: Data<Mutex<KVService>>, req: HttpRequest, json_request: web::Json<JSONRPCRequest>) -> JSONRPCResponse {
     handle(svc, &json_request).await
 }
 
 async fn handle(svc: Data<Mutex<KVService>>, json_request: &Json<JSONRPCRequest>) -> JSONRPCResponse {
     match json_request.method.as_str() {
-        "get" => JSONRPCResponse::Ok(JSONRPCOkResponse {
-            jsonrpc: "2.0".to_string(),
-            result: svc.lock().unwrap().get(&json_request.params).await.ok().unwrap_or("ERR".to_string()),
-            id: json_request.id,
-        }),
-        "set" => JSONRPCResponse::Ok(JSONRPCOkResponse {
-            jsonrpc: "2.0".to_string(),
-            result: svc.lock().unwrap().set(&json_request.params).await.ok().unwrap_or("ERR".to_string()),
-            id: json_request.id
-        }),
-        _ => JSONRPCResponse::Err(JSONRPCErrorResponse {
-            jsonrpc: "2.0".to_string(),
-            id: json_request.id,
-            error: JSONRPCErrorDetails {
-                code: -32601,
-                message: "Method not available".to_string(),
-            }
-        })
+        "get" => handle_get(&svc, &json_request).await,
+        "set" => handle_set(&svc, &json_request).await,
+        _ => rpc_error(json_request.id, -32601, "Method not available")
     }
+}
+
+async fn handle_set(svc: &Data<Mutex<KVService>>, json_request: &&Json<JSONRPCRequest>) -> JSONRPCResponse {
+    if json_request.params.len() != 2 {
+        return rpc_error(json_request.id, -32602, "Two parameters are required for set function")
+    }
+    let result = svc.lock().unwrap().set(&json_request.params).await;
+    if let Ok(value) = result {
+        return JSONRPCResponse::Ok(JSONRPCOkResponse {
+            jsonrpc: "2.0".to_string(),
+            result: value,
+            id: json_request.id,
+        });
+    }
+    rpc_error(json_request.id, -32603, result.err().unwrap().as_str())
+}
+
+async fn handle_get(svc: &Data<Mutex<KVService>>, json_request: &&Json<JSONRPCRequest>) -> JSONRPCResponse {
+    if json_request.params.len() != 1 {
+        return rpc_error(json_request.id, -32602, "One parameter is required for set function")
+    }
+
+    JSONRPCResponse::Ok(JSONRPCOkResponse {
+        jsonrpc: "2.0".to_string(),
+        result: svc.lock().unwrap().get(&json_request.params).await.ok().unwrap_or("ERR".to_string()),
+        id: json_request.id,
+    })
 }
